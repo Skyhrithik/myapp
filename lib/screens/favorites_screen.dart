84225963694dc77148d6b05eb186f0559c69fbaa
Skyhrithik <gagansingh0195@gmail.com>
2025-08-14 00:13:05 +0530

    FullUpdated

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -9,22 +9,18 @@ class FavoritesPage extends StatelessWidget {
       appBar: AppBar(title: const Text("Favorites")),
       body: ListView.builder(
         itemCount: 5,
-        itemBuilder:
-            (context, index) => Card(
-              margin: const EdgeInsets.all(10),
-              child: ListTile(
-                leading: const Icon(Icons.favorite, color: Colors.red),
-                title: Text('Favorite Item ${index + 1}'),
-                subtitle: const Text('This is a favorite product'),
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.delete_outline,
-                    color: Colors.redAccent,
-                  ),
-                  onPressed: () {},
-                ),
-              ),
+        itemBuilder: (context, index) => Card(
+          margin: const EdgeInsets.all(10),
+          child: ListTile(
+            leading: const Icon(Icons.favorite, color: Colors.red),
+            title: Text('Favorite Item ${index + 1}'),
+            subtitle: const Text('This is a favorite product'),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete_outline, color: Colors.redAccent),
+              onPressed: () {},
             ),
+          ),
+        ),
       ),
     );
   }
